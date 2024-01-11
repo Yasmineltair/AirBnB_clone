f1a8bc182ac1e9cc459850292581600f765cef91
Yasmineltair <yasmineltair293@gmail.com>
2024-01-11 13:16:55 -0800

    file_storage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 
+contain class FileStorage that serializes instances to a JSON
+file and deserializes JSON file to instances
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+	""" serializes instances to a JSON file and deserializes JSON file to instances"""
